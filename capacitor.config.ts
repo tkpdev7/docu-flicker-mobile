d6d23ac90e947ff271c7f96ea2720edfac96daaf
Dharani kiran <dharanicp77@gmail.com>
2025-04-01 21:39:22 +0530

    trying to debug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.59640a2f467f4711b707151b0c577cd5',
-  appName: 'docu-flicker-mobile',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: "https://59640a2f-467f-4711-b707-151b0c577cd5.lovableproject.com?forceHideBadge=true",
-    cleartext: true
-  },
+  appId: 'com.lovable.app',
+  appName: 'dharaniflicker',
+  webDir: 'dist'
 };
 
 export default config;
